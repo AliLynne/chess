81bf66f497ddb65228ab9919e5fcdef7c574a33a
Ali Thompson <alilynne.thompson@gmail.com>
2017-09-17 23:44:51 +0000

    prettified buttons

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -20,9 +20,14 @@ class GamesController < ApplicationController
 
   def update
     @game = Game.find_by_id(params[:id])
-    
-    @game.update_attributes(:black_player_id => current_user.id, :status => "in_progress")
-    redirect_to game_path(@game.id)
+    white_player = @game.white_player_id
+    if current_user.id != white_player
+      @game.update_attributes(:black_player_id => current_user.id, :status => "in_progress")
+      redirect_to game_path(@game.id)
+    else
+      redirect_to root_path
+      
+    end
   end
 
   def show
